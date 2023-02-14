33502b9e8d1bd21d0bd151d139f2ef51af3ea4ff
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2023-02-14 01:35:27 +0100

    initial stages of pokemon parsing pipeline

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/NordGus/gokedex/pokeapi"
@@ -14,13 +18,70 @@ func main() {
 	}
 
 	apiClient := pokeapi.NewClient(&client)
+	limit := 20
+	output := make(chan pokeapi.DetailPokemonResponse)
+	resultCount := 0
 
-	data, err := apiClient.ListPokemon(uint(0), uint(20))
+	go func(client pokeapi.Client, output chan pokeapi.DetailPokemonResponse, limit int) {
+		var wg sync.WaitGroup
+
+		for offset := 0; ; offset += limit {
+			data, err := client.ListPokemon(uint(offset), uint(limit))
+			if err != nil {
+				panic(err.Error())
+			}
+
+			resultCount = int(data.Count)
+
+			for _, result := range data.Results {
+				wg.Add(1)
+				go getPokemon(&wg, client, result, output)
+			}
+
+			if data.Next == "" {
+				wg.Wait()
+				close(output)
+				break
+			}
+		}
+	}(apiClient, output, limit)
+
+	count := 0
+
+	for range output {
+		count++
+	}
+
+	fmt.Println(count)
+	fmt.Println(resultCount)
+	fmt.Println(count == resultCount)
+}
+
+func getPokemon(wg *sync.WaitGroup, client pokeapi.Client, data pokeapi.ListPokemonResponseResult, out chan<- pokeapi.DetailPokemonResponse) {
+	defer wg.Done()
+
+	u, err := url.Parse(data.Url)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	for _, result := range data.Results {
-		fmt.Printf("\tName: %v,\tUrl: %v\n", result.Name, result.Url)
+	path := []string{}
+
+	for _, s := range strings.Split(u.Path, "/") {
+		if s != "" {
+			path = append(path, s)
+		}
 	}
+
+	id, err := strconv.Atoi(path[len(path)-1])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	result, err := client.GetPokemon(uint(id))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	out <- result
 }
--- pokeapi/client.go
+++ pokeapi/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 )
 
 type Connection interface {
@@ -29,6 +30,14 @@ type ListPokemonResponseResult struct {
 	Url  string `json:"url"`
 }
 
+type DetailPokemonResponse struct {
+	ID     uint64 `json:"id"`
+	Name   string `json:"name"`
+	Order  int64  `json:"order"`
+	Height uint64 `json:"height"`
+	Weight uint64 `json:"weight"`
+}
+
 func NewClient(conn Connection) Client {
 	return Client{
 		conn:    conn,
@@ -36,10 +45,47 @@ func NewClient(conn Connection) Client {
 	}
 }
 
-func (c *Client) ListPokemon(offset uint, limit uint) (data ListPokemonResponse, err error) {
-	url := fmt.Sprintf("%s/pokemon", c.baseUrl)
+func (c *Client) ListPokemon(offset uint, limit uint) (ListPokemonResponse, error) {
+	var data ListPokemonResponse
 
-	resp, err := c.conn.Get(url)
+	query := map[string]string{
+		"offset": fmt.Sprintf("%v", offset),
+		"limit":  fmt.Sprintf("%v", limit),
+	}
+
+	u, err := c.parseUrl("/pokemon", query)
+	if err != nil {
+		return data, err
+	}
+
+	resp, err := c.conn.Get(u)
+	if err != nil {
+		return data, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return data, err
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, err
+}
+
+func (c *Client) GetPokemon(id uint) (DetailPokemonResponse, error) {
+	var data DetailPokemonResponse
+
+	u, err := c.parseUrl(fmt.Sprintf("/pokemon/%v", id), map[string]string{})
+	if err != nil {
+		return data, err
+	}
+
+	resp, err := c.conn.Get(u)
 	if err != nil {
 		return data, err
 	}
@@ -56,4 +102,21 @@ func (c *Client) ListPokemon(offset uint, limit uint) (data ListPokemonResponse,
 	}
 
 	return data, nil
+}
+
+func (c *Client) parseUrl(path string, query map[string]string) (string, error) {
+	u, err := url.Parse(fmt.Sprintf("%v%v", c.baseUrl, path))
+	if err != nil {
+		return "", err
+	}
+
+	q := u.Query()
+
+	for k, v := range query {
+		q.Set(k, v)
+	}
+
+	u.RawQuery = q.Encode()
+
+	return u.String(), nil
 }
